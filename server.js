407baa90cb61cecdfe32b001a9b2fd2b32a99874
James Gerstenberger <gerstej9@gmail.com>
2021-01-04 21:00:15 -0800

    Graphql initial numerapi call for round close time set up, working on promises for return values

--- server.js
+++ server.js
@@ -1,83 +1,43 @@
-// const express = require('express');
-
-// const { graphqlHTTP } = require('express-graphql');
-// const { buildSchema} = require('graphql');
-// require('dotenv').config();
-// const PORT = process.env.PORT || 9999;
-
-// // app.use(express.static('./public'));
-// // app.use(express.urlencoded({extended:true}));
-// // app.set('view engine', 'ejs');
-
-// // app.get('/', getHomepage);
-
-// const schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-// const root = {
-//   hello: () =>{
-//     return 'Hello world';
-//   },
-// };
-
-// // function getHomepage(req,res){
-// //   res.render('index.ejs');
-// // }
-
-// const app = express();
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
-
-// app.use('*', (req, res) => res.status(404).send('Route you are looking for is not available'));
-
-// app.listen(PORT,() => console.log(`Listening on: ${PORT}`));
-
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+require('dotenv').config();
 const fetch = require('node-fetch');
-
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+const PORT = process.env.PORT || 9999;
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
 
-function retrieveObject(){
-  fetch('http://localhost:4000/graphql', {
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+app.get('/', getHomepage);
+
+function getHomepage(req,res){
+  const roundCloseDate =  retrieveObject(roundResolve(200));
+  console.log(roundCloseDate);
+  res.render('index.ejs', {roundCloseDate: roundCloseDate});
+}
+
+function roundResolve(round){
+  return `{rounds(number: ${round}) { resolveTime }}`;
+}
+
+function retrieveObject(Numquery){
+  const returnedInfo =  fetch('https://api-tournament.numer.ai/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    body: JSON.stringify({query: "{ hello }"})
+    body: JSON.stringify({
+      query: Numquery})
   })
     .then(r => r.json())
-    .then(data => console.log('data returned:', data));
+    .then(data => {console.log('data returned:', data.data);
+    });
+  return returnedInfo;
 }
 
-retrieveObject();
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.use('*', (req, res) => res.status(404).send('Route you are looking for is not available'));
+
+app.listen(PORT,() => console.log(`Listening on: ${PORT}`));
