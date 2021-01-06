9b972535cc3a2c8077fab3cac148b5c919fc02ab
James Gerstenberger <gerstej9@gmail.com>
2021-01-05 21:19:32 -0800

    Created daily horse race prototype/ daily model analysis on server and implemented basic display on index.ejs

--- query.js
+++ query.js
@@ -1,24 +1,24 @@
 // query{
-//   v2UserProfile(username:"gerstej9") {
-//     latestRanks {
-//       mmcRank
-//       prevMmcRank
-//       prevRank
-//       rank
-//     }
-//     latestRoundPerformances {
-//       correlation
-//       correlationWithMetamodel
-//       date
-//       mmc
-//       payoutPending
-//       payoutSettled
-//       roundNumber
-//       roundResolved
-//       selectedStakeValue
-//       weekPayoutSelection
-//     }
-//     totalStake
-//     username
-//   }
+  // v2UserProfile(username:"gerstej9") {
+  //   latestRanks {
+  //     mmcRank
+  //     prevMmcRank
+  //     prevRank
+  //     rank
+  //   }
+  //   latestRoundPerformances {
+  //     correlation
+  //     correlationWithMetamodel
+  //     date
+  //     mmc
+  //     payoutPending
+  //     payoutSettled
+  //     roundNumber
+  //     roundResolved
+  //     selectedStakeValue
+  //     weekPayoutSelection
+  //   }
+  //   totalStake
+  //   username
+  // }
 // }
--- server.js
+++ server.js
@@ -11,17 +11,45 @@ app.set('view engine', 'ejs');
 
 app.get('/', getHomepage);
 
-const roundResolve = round => `{rounds(number: ${round}) { resolveTime }}`;
+// const roundResolve = round => `{rounds(number: ${round}) { resolveTime }}`;
 const latestNmrPrice = () => '{latestNmrPrice {lastUpdated PriceUSD}}';
+const userProfile = (username) => `{v2UserProfile(username: "${username}") {
+  latestRanks {
+    mmcRank
+    prevMmcRank
+    prevRank
+    rank
+  }
+  latestRoundPerformances {
+    correlation
+    correlationWithMetamodel
+    date
+    mmc
+    payoutPending
+    payoutSettled
+    roundNumber
+    roundResolved
+    selectedStakeValue
+    weekPayoutSelection
+  }
+  dailyUserPerformances {
+    payoutPending
+  }
+  totalStake
+  username
+}}`;
 
 
 async function getHomepage(req,res){
-  const [roundClose, nmrPrice] = await Promise.all([
-    retrieveObject(roundResolve(190)), 
-    retrieveObject(latestNmrPrice())
+  const [currentNmr, userData] = await Promise.all([
+    // retrieveObject(roundResolve(190)),
+    retrieveObject(latestNmrPrice()),
+    horse_race('gerstej9')
   ]);
-  const roundCloseDate = roundClose.rounds[0].resolveTime;
-  res.render('index.ejs', {roundCloseDate: roundCloseDate, nmrPrice: nmrPrice.latestNmrPrice.PriceUSD});
+  // const roundCloseDate = roundClose.rounds[0].resolveTime;
+  const nmrPrice = Number(currentNmr.latestNmrPrice.PriceUSD);
+  // console.log(userData);
+  res.render('index.ejs', {nmrPrice: nmrPrice.toFixed(2), userData: userData, latestRounds: userData[4]});
 }
 
 
@@ -43,8 +71,25 @@ async function retrieveObject(numquery){
   return returnedInfo;
 }
 
+async function horse_race(username){
+  const user = await retrieveObject(userProfile(username));
+  const [userMmcRankCurrent, userMmcRankPrev, userCorrCurrent, userCorrPrev, activeRounds, totalStake, modelName, dailyChange] =
+  [
+    user.v2UserProfile.latestRanks.mmcRank,
+    user.v2UserProfile.latestRanks.prevMmcRank,
+    user.v2UserProfile.latestRanks.rank,
+    user.v2UserProfile.latestRanks.prevRank,
+    user.v2UserProfile.latestRoundPerformances.slice(-4),
+    Number(user.v2UserProfile.totalStake).toFixed(2),
+    user.v2UserProfile.username,
+    Number(user.v2UserProfile.dailyUserPerformances[0].payoutPending).toFixed(2)
+  ];
+  // console.log(user.v2UserProfile.dailyUserPerformances[0]);
+  // console.log(user.v2UserProfile.latestRoundPerformances.slice(-4));
+  return[userMmcRankCurrent, userMmcRankPrev, userCorrCurrent, userCorrPrev, activeRounds, totalStake, modelName, dailyChange];
+}
 
-
+horse_race("gerstej9");
 // retrieveObject(latestNmrPrice())
 //   .then(result => console.log(result));
 
