1d4f69a3e707100d02fe4108c8a39e0d2e7168ac
James Gerstenberger <gerstej9@gmail.com>
2021-01-05 19:23:37 -0800

    sample query for user information

---
+++ query.js
@@ -0,0 +1,24 @@
+// query{
+//   v2UserProfile(username:"***REMOVED***") {
+//     latestRanks {
+//       mmcRank
+//       prevMmcRank
+//       prevRank
+//       rank
+//     }
+//     latestRoundPerformances {
+//       correlation
+//       correlationWithMetamodel
+//       date
+//       mmc
+//       payoutPending
+//       payoutSettled
+//       roundNumber
+//       roundResolved
+//       selectedStakeValue
+//       weekPayoutSelection
+//     }
+//     totalStake
+//     username
+//   }
+// }
