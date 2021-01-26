40284881588cb0b0d4e8d4417224c90a236be605
James Gerstenberger <gerstej9@gmail.com>
2021-01-26 14:25:59 -0800

    General clean up of code and folders, created readme content, created aboutus page

--- server.js
+++ server.js
@@ -20,14 +20,10 @@ app.use(cookies());
 
 
 //Global Constant
-const latestRoundsPosition = 4;
 let modelFound = true;
-// localStorage.setItem('modelCollections', modelCollections);
-
 
 //Query Constants
 
-// const roundResolve = round => `{rounds(number: ${round}) { resolveTime }}`;
 const latestNmrPrice = () => '{latestNmrPrice {lastUpdated PriceUSD}}';
 const userProfile = username => `{v2UserProfile(username: "${username}") {
   latestRanks {
@@ -74,13 +70,10 @@ const v2RoundDetails = roundNumber => `{
 
 // Route Paths
 app.get('/', getHomepage);
+app.get('/about_us', getAboutUs);
 app.post('/user', getUserName);
-// app.post('/newuser', createNewUser);
 app.get('/detail/:user', getModelDetails);
 app.get('/percent', getPercentile);
-// app.put('/:username/addmodel', userAddModel);
-app.put('/:username/removemodel',userRemoveModel);
-app.get('/:user/model_not_found', modelNotFound);
 app.get('/:user/modelcomparison', ModelComparison);
 app.get('/:user/download', downloadSQL);
 
@@ -108,8 +101,6 @@ function NewScore(model, corr, mmc, newscore, corrPassing, newScorePassing){
 
 
 //Helper Functions
-
-
 const sortUsersCorr = (leftModel, rightModel) =>{
   if(leftModel.corr < rightModel.corr){
     return 1;
@@ -164,7 +155,6 @@ async function retrieveObject(numquery){
     .then(r => r.json())
     .then(data => data.data)
     .catch(error => console.error(error));
-  // console.log(returnedInfo);
   return returnedInfo;
 }
 
@@ -177,30 +167,10 @@ async function getPercentile(roundNumber, modelArr){
   const corrArr = filteredArr.map(user => user.correlation);
   const ninentyPercentile = percentileValue(corrArr, 80);
   const userModelArr = filteredArr.filter(user => modelArr.includes(user.username));
-  // console.log(roundNumber);
   const userNinety = userModelArr.filter(user => user.correlation > ninentyPercentile);
   userNinety.sort(sortUsersCorr);
   const userFinalArr = userNinety.map(model => model.username);
   return {value: ninentyPercentile, userArr: userFinalArr};
-}
-
-//Individual user profile information retrieval
-async function modelComp(username){
-  const user = await retrieveObject(userProfile(username));
-  const [userMmcRankCurrent, userMmcRankPrev, userCorrCurrent, userCorrPrev, activeRounds, totalStake, modelName, dailyChange] =
-  [
-    user.v2UserProfile.latestRanks.mmcRank,
-    user.v2UserProfile.latestRanks.prevMmcRank,
-    user.v2UserProfile.latestRanks.rank,
-    user.v2UserProfile.latestRanks.prevRank,
-    user.v2UserProfile.latestRoundPerformances.slice(-4),
-    Number(user.v2UserProfile.totalStake).toFixed(2),
-    user.v2UserProfile.username,
-    Number(user.v2UserProfile.dailyUserPerformances[0].payoutPending).toFixed(2)
-  ];
-  // console.log(user.v2UserProfile.dailyUserPerformances[0]);
-  // console.log(user.v2UserProfile.latestRoundPerformances.slice(-4));
-  return[userMmcRankCurrent, userMmcRankPrev, userCorrCurrent, userCorrPrev, activeRounds, totalStake, modelName, dailyChange];
 }
 
 
@@ -209,11 +179,13 @@ function getHomepage(req, res){
   res.render('pages/home.ejs', {userExist: 'none', theme: getTheme(req), modelExistsUser: false, modelName: false});
 }
 
+function getAboutUs(req, res){
+  res.render('pages/about_us.ejs', {theme: getTheme(req)});
+}
+
 function getUserName(req, res){
-  // console.log(req.body.username);
   client.query(`SELECT * FROM userProfile WHERE username = '${req.body.username}'`)
     .then(result =>{
-      // console.log(result.rows[0]);
       if(result.rows[0] === undefined){
         res.render('pages/home.ejs', {userExist: 'no', theme: getTheme(req)});
       }else{
@@ -222,44 +194,6 @@ function getUserName(req, res){
     });
 }
 
-// function createNewUser(req, res){
-//   const theme = req.cookies.theme;
-//   client.query(`SELECT * FROM userProfile WHERE username = '${req.body.username}'`)
-//     .then(result =>{
-//       if(result.rows[0] !== undefined){
-//         res.render('pages/home.ejs', {userExist: 'yes', theme: theme});
-//       }else{
-//         client.query(`INSERT INTO userProfile (username, models) VALUES('${req.body.username}', ARRAY ['${req.body.model}'])`)
-//           .then(() => {
-//             res.redirect(`/detail/${req.body.username}`);
-//           });
-//       }
-//     });
-// }
-
-// function createNewUser(req, res){
-//   const newCollection = req.body.username;
-//   const LSmodels = localStorage.getItem('modelCollections');
-//   const modelCollections = JSON.parse(LSmodels);
-//   if(modelCollections[0].collectionName === newCollection){
-//     res.render('pages/home.ejs', {userExist: 'yes', theme: getTheme(req)});
-//   }else{
-//     res.cookie('username', `${newCollection}`);
-//     res.redirect(`/detail/${newCollection}`);
-//   }
-// }
-
-async function retrieveUserModels(user){
-  let modelArr = [];
-  await client.query(`SELECT * FROM userProfile WHERE username = '${user}' `)
-    .then(result => {
-      // console.log(result.rows[0]);
-      modelArr = result.rows[0].models;
-      // console.log(modelArr);
-      return modelArr;
-    });
-  return modelArr;
-}
 
 
 //Model Detail Page
@@ -270,8 +204,6 @@ async function getModelDetails(req,res){
   }
   const username = req.params.user;
   modelFound = true;
-  // const modelArr = await retrieveUserModels(username);
-  // console.log(modelArr);
   const userModelArr = await multiHorse(modelArray);
   if(userModelArr[0] === false){
     res.render('pages/home.ejs', {userExist: 'none', theme: getTheme(req), modelExistsUser: username, modelName: userModelArr[1]});
@@ -299,8 +231,6 @@ async function multiHorse(arr){
         user.v2UserProfile.username,
         Number(user.v2UserProfile.dailyUserPerformances[0].payoutPending).toFixed(2)
       ];
-      // console.log(user.v2UserProfile.dailyUserPerformances[0]);
-      // console.log(user.v2UserProfile.latestRoundPerformances.slice(-4));
       userModelArr.push(new UserDetail(userMmcRankCurrent, userMmcRankPrev, userCorrCurrent, userCorrPrev, activeRounds, totalStake, modelName, dailyChange));
     }
     catch(error){
@@ -311,7 +241,6 @@ async function multiHorse(arr){
 }
 
 async function ModelComparison(req, res){
-  // array = comparison array
   const username = req.params.user;
   let modelArray = req.query.model;
   if(typeof(modelArray) === 'string'){
@@ -329,7 +258,6 @@ async function ModelComparison(req, res){
     let newscore = Number(mmc)+Number(corr);
     return new NewScore(modelName, corr, mmc, newscore);
   });
-  // console.log(newScoreArr);
   let sortedArray = newScoreArr.sort(sortUsersCorr);
   client.query(`DELETE FROM ModelData WHERE round = ${round} AND username = '${username}'`);
   sortedArray.forEach(model =>{
@@ -343,19 +271,8 @@ async function ModelComparison(req, res){
   res.render('pages/newscore.ejs', {userData: bottomArr, ninetyModelArr: topPercentileArr, date: date, round: round, theme: getTheme(req)});
 }
 
-
-// async function downloadSQL(req, res){
-//   const username = req.params.user;
-//   await client.query(`\copy (SELECT round, model, corr, percentileAllModelCorr, passingPercentile, percentileValue, mmc, newscore FROM ModelData WHERE username = '${username}') TO '/tmp/numerai_comparison.csv' csv header`);
-//   res.download('/tmp/numerai_comparison.csv');
-// }
-// round	model	corr	percentileallmodelcorr	passingpercentile	percentilevalue	mmc	newscore
-// 242	***REMOVED***	0.013594668	80	TRUE	0.029695973	0.012317861	0.025912528
-// 242	***REMOVED***	0.013335187	80	FALSE	0.029695973	0.012128109	0.025463296
-
 async function downloadSQL(req, res){
   const username = req.params.user;
-
   const result = await client.query(`SELECT round, model, corr, percentileAllModelCorr, passingPercentile, percentileValue, mmc, newscore FROM ModelData WHERE username = '${username}'`);
   const rows = result.rows;
   console.log(rows);
@@ -363,7 +280,6 @@ async function downloadSQL(req, res){
   const dataRows = rows.reduce(function (csvDataString, row) {
     csvDataString += `"${row.round}","${row.model}","${row.corr}","${row.percentileallmodelcorr}","${row.passingpercentile}","${row.percentilevalue}","${row.mmc}","${row.newscore}"`;
     csvDataString += '\r\n';
-
     return csvDataString;
   }, '');
   res.header('Content-Type', 'text/csv');
@@ -371,53 +287,19 @@ async function downloadSQL(req, res){
   return res.send(headerRow + dataRows);
 }
 
-async function userAddModel(req, res){
-  const username = req.params.username;
-  const newModel = req.body.model;
-  let test = await retrieveObject(userProfile(`${newModel}`));
-  console.log(test.v2UserProfile);
-  if(test.v2UserProfile === null){
-    res.redirect(`/${username}/model_not_found`);
-  }else{
-    await client.query(`UPDATE userProfile SET models = models || '{${newModel}}' WHERE username = '${username}'`);
-    res.redirect(`/detail/${username}`);
-  }
-}
 
-async function modelNotFound(req, res){
-  const username = req.params.user;
-  const modelArr = await retrieveUserModels(username);
-  const userModelArr = await multiHorse(modelArr);
-  const currentNmr = await retrieveObject(latestNmrPrice());
-  const nmrPrice = Number(currentNmr.latestNmrPrice.PriceUSD);
-  const date = userModelArr[0].activeRounds[3].date.substring(0,10);
-  modelFound = false;
-  res.render('pages/userDetails.ejs', {nmrPrice: nmrPrice.toFixed(2), userData: userModelArr, date: date, username: username, modelFound: modelFound, theme: getTheme(req)});
-}
-
-async function userRemoveModel(req, res){
-  const username = req.params.username;
-  const removeModel = req.body.model;
-  // console.log(username);
-  client.query(`SELECT * FROM userProfile WHERE username = '${username}'`)
-    .then(result => console.log(result.rows));
-  await client.query(`UPDATE userProfile SET models = array_remove(models, '${removeModel}') WHERE username = '${username}'`);
-  res.redirect(`/detail/${username}`);
-}
 
 // Function to retrieve list of all users from leaderboard
 async function getUsers(){
   const leaderboard = await retrieveObject(v2Leaderboard());
   const leaderboardUsers = leaderboard.v2Leaderboard;
   let users = leaderboardUsers.map(user => user.username);
-  // console.log(users);
   return users;
 }
 
 // Function for retrieving mmc of a user
 async function userProfileMmc(username, round){
   const user = await retrieveObject(userProfile(username));
-  // console.log(user);
   const userRounds = user.v2UserProfile.latestRoundPerformances;
   const mmcRound = userRounds.filter(userRound => userRound.roundNumber === round);
   const targetMmc = mmcRound[0].mmc;
@@ -439,8 +321,6 @@ async function calculateRoundInfo(round, modelArr){
     }
   }
   const filteredMmcArr = mmcArr.filter(m => m !== null);
-  // console.log(filteredMmcArr);
-  // console.log(modelMmcArr);
   const ninentyPercentile = percentileValue(filteredMmcArr, 90);
   console.log(round);
   const userNinety = modelMmcArr.filter(user => user.mmc > ninentyPercentile);
@@ -451,17 +331,17 @@ async function calculateRoundInfo(round, modelArr){
 
 
 //Executable functions
-
-
-//TODO uncouple model new score and c reate for each individual
-//Change pocketmonkey to monkey
-
-
 app.use('*', (req, res) => res.status(404).send('Route you are looking for is not available'));
 
 client.connect().then(() => {
   app.listen(PORT,() => console.log(`Listening on: ${PORT}`));
 });
+
+
+
+
+
+
 
 //Get every model for every round
 //Get rid of rest of database
@@ -472,13 +352,11 @@ client.connect().then(() => {
 
 //TODO
 //NEW postsgres table for historic graphql data
-//Catch for wrong models that redirects to homepage with indication of wrong model
-//Avg corr for model of all live rounds
 //Avg corr for all models across all live rounds
 //Avg corr for all models across individual round
 //Slope for each model over previous 12 closed rounds
 //README
 //About us page
-//HEROKU link in github description
-//Clean up code
+//Add comments and rename variables to be clearer
 //CSS
+//Create Footer
